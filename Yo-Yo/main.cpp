22fc1d685ecffc8cc17dff66c682f44d889afda9
TatianaGaintseva <novacaine833@gmail.com>
2014-11-23 21:50:31 +0500

    Update main.cpp

--- Yo-Yo/main.cpp
+++ Yo-Yo/main.cpp
@@ -4,18 +4,20 @@
 
 int main(int argc, char* argv[]) {
 
+    TAlgo algo;
+
     if (argc == 3 && std::string(argv[1]) == "index") {
-        process_index(std::string(argv[2]));
+        algo.ProcessIndex(std::string(argv[2]));
     } 
     else if (argc >= 3 && std::string(argv[1]) == "find") {
 	std::vector<std::string> arguments;
 	for (int i = 2; i < argc; ++i) {
 	    arguments.push_back(std::string(argv[i]));
 	}
-        process_find(arguments);  
+        algo.ProcessFind(arguments);  
     } 
     else if (argc == 2 && std::string(argv[1]) == "--help") {
-        process_help();
+        algo.ProcessHelp();
     } 
     else {
         std::cout << "Usage: " + std::string(argv[0]) + " --help" << '\n';
