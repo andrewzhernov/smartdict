1e5d613e6b9817651d7dabd622472be19fd143d6
TatianaGaintseva <novacaine833@gmail.com>
2014-11-23 21:48:12 +0500

    Update algo.cpp

--- Yo-Yo/algo.cpp
+++ Yo-Yo/algo.cpp
@@ -1,22 +1,21 @@
 #include "algo.h"
 
 
-
-void process_index(std::string argument) {
-	TIndexer indexer;
+void TAlgo::ProcessIndex(std::string argument) {
+    TIndexer indexer;
     indexer.IndexFile(argument);
     indexer.SaveToDisk(INDEX_FILE);
 }
 
-void process_find(std::vector<std::string> arguments) {
-	std::vector<std::string> sentences;
+void TAlgo::ProcessFind(std::vector<std::string> arguments) {
+    std::vector<std::string> sentences;
     TIndexer indexer;
     indexer.LoadFromDisk(INDEX_FILE);
     indexer.FindSentences(arguments, sentences);
     if (sentences.empty()) {
         std::cout << "Not found" << '\n';
     } 
-	else {
+    else {
         std::cout << SEPARATOR << '\n';
         for (size_t i = 0; i < sentences.size(); ++i) {
             std::cout << sentences[i] << '\n' << SEPARATOR << '\n';
@@ -24,12 +23,11 @@ void process_find(std::vector<std::string> arguments) {
     }
 }
 
-void process_help() {
-	std::cout << "AVAILABLE COMMANDS:" << '\n';
+void TAlgo::ProcessHelp() {
+    std::cout << "AVAILABLE COMMANDS:" << '\n';
     std::cout << PROGRAMNAME << " index SOURCE_FILE" << '\n';
-    std::cout << PROGRAMNAME << " find WORD" << '\n';
+    std::cout << PROGRAMNAME << " find WORDS" << '\n';
 }
-
 
 
 
