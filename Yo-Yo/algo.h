bc480796f7a253cba754c9d5c04a73924a50e7b3
TatianaGaintseva <novacaine833@gmail.com>
2014-11-23 21:49:25 +0500

    Update algo.h

--- Yo-Yo/algo.h
+++ Yo-Yo/algo.h
@@ -5,15 +5,16 @@
 #include <vector>
 #include "indexer.h"
 
-const std::string INDEX_FILE = "..Yo-Yo/index";
+const std::string INDEX_FILE = "/home/tatiana/Yo-Yo/index";
 const std::string SEPARATOR = "-----------------------";
 const std::string PROGRAMNAME = "SmartDict";
 
-void process_index(std::string argument);
 
-void process_find(std::vector<std::string> argument);
-
-void process_help();
-
+class TAlgo {
+public:
+    void ProcessIndex(std::string argument);
+    void ProcessFind(std::vector<std::string> argument);
+    void ProcessHelp();
+};
 
 #endif /* ALGO_H_ */
