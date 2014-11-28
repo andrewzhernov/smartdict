bffeb8123794b13e332c1b9546e325d8d5ad1ded
TatianaGaintseva <novacaine833@gmail.com>
2014-11-29 03:39:42 +0500

    Update algo.cpp

--- Yo-Yo/algo.cpp
+++ Yo-Yo/algo.cpp
@@ -2,20 +2,20 @@
 
 
 void TAlgo::ProcessIndex(const std::string argument) {
-	TIndexer indexer;
+    TIndexer indexer;
     indexer.IndexFile(argument);
     indexer.SaveToDisk(INDEX_FILE);
 }
 
 void TAlgo::ProcessFind(const std::vector<std::string> arguments, const std::vector<std::string> arguments_minus) {
-	std::vector<std::string> sentences;
+    std::vector<std::string> sentences;
     TIndexer indexer;
     indexer.LoadFromDisk(INDEX_FILE);
     indexer.FindSentences(arguments, arguments_minus, sentences);
     if (sentences.empty()) {
         std::cout << "Not found" << '\n';
     } 
-	else {
+    else {
         std::cout << SEPARATOR << '\n';
         for (size_t i = 0; i < sentences.size(); ++i) {
             std::cout << sentences[i] << '\n' << SEPARATOR << '\n';
@@ -24,7 +24,7 @@ void TAlgo::ProcessFind(const std::vector<std::string> arguments, const std::vec
 }
 
 void TAlgo::ProcessHelp() {
-	std::cout << "AVAILABLE COMMANDS:" << '\n';
+    std::cout << "AVAILABLE COMMANDS:" << '\n';
     std::cout << PROGRAMNAME << " index SOURCE_FILE" << '\n';
     std::cout << PROGRAMNAME << " find WORDS" << '\n';
 }
