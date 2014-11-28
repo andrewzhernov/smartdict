66ac2a38a968a6e97a600468d24e91885ccc6f03
TatianaGaintseva <novacaine833@gmail.com>
2014-11-29 03:45:03 +0500

    Update indexer.cpp

--- Yo-Yo/indexer.cpp
+++ Yo-Yo/indexer.cpp
@@ -18,12 +18,11 @@ void TIndexer::IndexText(const std::string& text) {
     TParser parser;
     parser.SplitText(text, sentences);
     for (const auto& sentence : sentences) {
+	int next_id = SentenceIndex.size();
+	if(SentenceIndex.find(sentence) != SentenceIndex.end()) continue;
 
-		int next_id = SentenceIndex.size();
-		if(SentenceIndex.find(sentence) != SentenceIndex.end()) continue;
-
-		SentenceIndex.insert(std::pair<std::string, int>(sentence, Sentences.size()));
-		Sentences.push_back(sentence);
+	SentenceIndex.insert(std::pair<std::string, int>(sentence, Sentences.size()));
+	Sentences.push_back(sentence);
     	parser.SplitSentence(sentence, words);
 
     	for (const auto& word : words) IndexSentence(word, sentence, sentences.size());
@@ -38,66 +37,64 @@ void TIndexer::IndexFile(const std::string& filename) {
 }
 
 int TIndexer::FindBlocksIDs(const std::vector<std::string> words, std::vector<int>& ids) const {
-	int return_value = 0;
-	for (const auto& word : words) {
-		auto it = WordIndex.find(word);
-		if (it == WordIndex.end()) {
-			return_value = 1;
-			continue;
-		}
-		ids.push_back(it->second);
+    int return_value = 0;
+    for (const auto& word : words) {
+	auto it = WordIndex.find(word);
+	if (it == WordIndex.end()) {
+	    return_value = 1;
+	    continue;
 	}
-	return return_value;
+	ids.push_back(it->second);   
+     }
+     return return_value;
 }
 
 void TIndexer::FindSentences(const std::vector<std::string> words, const std::vector<std::string> words_minus, 
-							std::vector<std::string>& sentences) const {
+			    std::vector<std::string>& sentences) const {
 
-	std::vector<int> ids;
-	std::vector<int> ids_minus;
-	FindBlocksIDs(words_minus, ids_minus);
-	if (FindBlocksIDs(words, ids)) {
-		sentences.clear();
-		return;
-	}
-	boost::dynamic_bitset<> right_block = BlockOfSentences[ids[0]];
-	for (const auto& id : ids) {
-		boost::dynamic_bitset<> next_block = BlockOfSentences[id];
-		right_block = right_block & next_block;
-	}
+    std::vector<int> ids;
+    std::vector<int> ids_minus;
+    FindBlocksIDs(words_minus, ids_minus);
+    if (FindBlocksIDs(words, ids)) {
+	sentences.clear();
+	return;
+    }
+    boost::dynamic_bitset<> right_block = BlockOfSentences[ids[0]];
+    for (const auto& id : ids) {
+	boost::dynamic_bitset<> next_block = BlockOfSentences[id];
+	right_block = right_block & next_block;
+    }
 
-	if (ids_minus.size() > 0) {	
-		boost::dynamic_bitset<> false_block = BlockOfSentences[ids_minus[0]];
-		for (const auto& id : ids_minus) {
-			boost::dynamic_bitset<> next_block = BlockOfSentences[id];
-			false_block = false_block | next_block;
-		}
-
-		right_block = right_block ^ false_block;
+    if (ids_minus.size() > 0) {	
+	boost::dynamic_bitset<> false_block = BlockOfSentences[ids_minus[0]];
+	for (const auto& id : ids_minus) {
+	    boost::dynamic_bitset<> next_block = BlockOfSentences[id];
+       	    false_block = false_block | next_block;
 	}
+	right_block = right_block ^ false_block;
+    }
 
-	
-	for (int i = 0; i < right_block.size(); ++i) {
-		if (right_block[i]) sentences.push_back(Sentences[i]);
-	}
+    for (int i = 0; i < right_block.size(); ++i) {
+	if (right_block[i]) sentences.push_back(Sentences[i]);
+    }
 }
 
 
 void TIndexer::LoadFromDisk(const std::string& filename) {
     WordIndex.clear();
     BlockOfSentences.clear();
-	SentenceIndex.clear();
-	Sentences.clear();
+    SentenceIndex.clear();
+    Sentences.clear();
 
     size_t count, block_size;
     std::string word, sentence;
-	int sentence_index = 0;
-	int sentences_amount;
+    int sentence_index = 0;
+    int sentences_amount;
 
     std::ifstream ifs(filename);
     ifs >> count;
-	std::getline(ifs, word);
-	ifs>> sentences_amount;
+    std::getline(ifs, word);
+    ifs>> sentences_amount;
     std::getline(ifs, word);
     for (size_t i = 0; i < count; ++i) {
         std::getline(ifs, word);
@@ -109,15 +106,15 @@ void TIndexer::LoadFromDisk(const std::string& filename) {
         for (size_t j = 0; j < block_size; ++j) {
             std::getline(ifs, sentence);
 			
-			if (SentenceIndex.find(sentence) != SentenceIndex.end()) {
-				BlockOfSentences[i][SentenceIndex[sentence]] = 1;
-			}
-			else {
+	    if (SentenceIndex.find(sentence) != SentenceIndex.end()) {
+		BlockOfSentences[i][SentenceIndex[sentence]] = 1;
+       	    }
+	    else {
             	BlockOfSentences[i][sentence_index] = 1;
-				SentenceIndex.insert(std::pair<std::string, int>(sentence, sentence_index));
-				Sentences.push_back(sentence);
-				sentence_index++;
-			}
+		SentenceIndex.insert(std::pair<std::string, int>(sentence, sentence_index));
+		Sentences.push_back(sentence);
+		sentence_index++;
+	    }
         }
     }
 }
@@ -125,7 +122,7 @@ void TIndexer::LoadFromDisk(const std::string& filename) {
 void TIndexer::SaveToDisk(const std::string& filename) const {
     std::ofstream ofs(filename);
     ofs << WordIndex.size() << '\n';
-	ofs << SentenceIndex.size() << '\n';
+    ofs << SentenceIndex.size() << '\n';
 
     for (auto word_it = WordIndex.begin(); word_it != WordIndex.end(); ++word_it) {
         const boost::dynamic_bitset<> block = BlockOfSentences[word_it->second];
