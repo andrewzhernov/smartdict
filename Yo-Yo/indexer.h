af4202988679725300b1837a68e324c05ece83df
TatianaGaintseva <novacaine833@gmail.com>
2014-12-01 04:34:50 +0500

    Update indexer.h

--- Yo-Yo/indexer.h
+++ Yo-Yo/indexer.h
@@ -13,10 +13,10 @@
 
 class TIndexer {
 private:
-    std::map<std::string, size_t> WordIndex;
-    std::vector<boost::dynamic_bitset<> > BlockOfSentences;
-    std::map<std::string, int> SentenceIndex;
-    std::vector<std::string> Sentences;
+    std::map<std::string, size_t> word_index_;
+    std::vector<boost::dynamic_bitset<> > block_of_sentences_;
+    std::map<std::string, int> sentence_index_;
+    std::vector<std::string> sentences_;
 
     int FindBlocksIDs(const std::vector<std::string> words, std::vector<int>& ids) const;
     void IndexSentence(const std::string& word, const std::string& sentence, size_t sentences_size);
